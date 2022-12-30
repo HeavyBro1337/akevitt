221e06070a5443cd98152895e9b74cb900f8ae96
IvanKorchmit <criperadmiral92@gmail.com>
2022-12-31 00:58:41 +0200

    Wrapped functions and methods

--- playerdbmanipulation.go
+++ playerdbmanipulation.go
@@ -13,30 +13,44 @@ import (
 const BUCKET_USERS string = "Players"
 
 type Player struct {
+	ID       uint64
 	Username string
 	Password string
 }
 
+func (p Player) Save(db *bolt.DB) error {
+	errResult := db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(BUCKET_USERS))
+
+		if err != nil {
+			return err
+		}
+		serialized, err := p.Serialize()
+		if err != nil {
+			return err
+		}
+		b.Put(Int2Byte(p.ID), serialized)
+		return nil
+	})
+	return errResult
+}
 func (p Player) String() string {
 	return fmt.Sprintf("%s %s", p.Username, p.Password)
 }
 
 func CreatePlayer(db *bolt.DB, player Player) error {
 	errResult := db.Update(func(tx *bolt.Tx) error {
-		b, berr := tx.CreateBucketIfNotExists([]byte(BUCKET_USERS))
-		if berr != nil {
-			return berr
+		b, err := tx.CreateBucketIfNotExists([]byte(BUCKET_USERS))
+		if err != nil {
+			return err
 		}
-		var plrBuff bytes.Buffer
-		enc := gob.NewEncoder(&plrBuff) // Will write to network.
-		encodeErr := enc.Encode(player)
-		if encodeErr != nil {
-			return encodeErr
+		serialized, err := player.Serialize()
+		if err != nil {
+			return err
 		}
 		id, _ := b.NextSequence()
-		binaryId := make([]byte, 8)
-		binary.LittleEndian.PutUint64(binaryId, uint64(id))
-		b.Put(binaryId, plrBuff.Bytes())
+
+		b.Put(Int2Byte(id), serialized)
 		return nil
 	})
 	return errResult
@@ -44,14 +58,12 @@ func CreatePlayer(db *bolt.DB, player Player) error {
 func GetPlayer(id uint64, db *bolt.DB) Player {
 	var result Player
 	db.Update(func(tx *bolt.Tx) error {
-		b, berr := tx.CreateBucketIfNotExists([]byte(BUCKET_USERS))
-		if berr != nil {
-			return berr
+		b, err := tx.CreateBucketIfNotExists([]byte(BUCKET_USERS))
+		if err != nil {
+			return err
 		}
 		var decodeBuffer bytes.Buffer
-		binaryId := make([]byte, 8)
-		binary.LittleEndian.PutUint64(binaryId, uint64(id))
-		decodeBuffer.Write(b.Get(binaryId))
+		decodeBuffer.Write(b.Get(Int2Byte(id)))
 		dec := gob.NewDecoder(&decodeBuffer)
 		decErr := dec.Decode(&result)
 		if decErr != nil {
@@ -60,4 +72,21 @@ func GetPlayer(id uint64, db *bolt.DB) Player {
 		return nil
 	})
 	return result
+}
+
+// Converts Uint64 to byte array
+func Int2Byte(value uint64) []byte {
+	binaryId := make([]byte, 8)
+	binary.LittleEndian.PutUint64(binaryId, uint64(value))
+	return binaryId
+}
+
+func (p Player) Serialize() ([]byte, error) {
+	var plrBuff bytes.Buffer
+	enc := gob.NewEncoder(&plrBuff)
+	encodeErr := enc.Encode(p)
+	if encodeErr != nil {
+		return nil, encodeErr
+	}
+	return plrBuff.Bytes(), nil
 }
