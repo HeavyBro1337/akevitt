ab25368bf7469186710e9d54e1b572c6f6ce6aa2
IvanKorchmit <criperadmiral92@gmail.com>
2023-08-04 14:50:57 +0300

    Changed port

--- Scripts/Windows/connect.bat
+++ Scripts/Windows/connect.bat
@@ -1,4 +1,4 @@
 Rem This is basic connection via ssh. You don't have to clear known_hosts file each time you connect to the server
 
 
-ssh -o StrictHostKeyChecking=no localhost -p 25565
+ssh -o StrictHostKeyChecking=no localhost -p 1487
