8ba11c7fc1d536761107647be49542f18e876f02
IvanKorchmit <criperadmiral92@gmail.com>
2023-08-22 21:01:59 +0300

    commands reorganization

---
+++ examples/iron-exalt/commands.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"akevitt/akevitt"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func enter(engine *akevitt.Akevitt, session akevitt.ActiveSession, arguments string) error {
+	sess, ok := session.(*ActiveSession)
+	if !ok {
+		return errors.New("invalid session type")
+	}
+	character := sess.character
+	prevRoom := character.currentRoom.GetName()
+	roomKey, err := strconv.ParseUint(arguments, 10, 64)
+	if err != nil {
+		return err
+	}
+	exit, err := akevitt.IsRoomReachable[*Room](engine, session, roomKey, character.CurrentRoomKey)
+	if err != nil {
+		return err
+	}
+	err = exit.Enter(engine, session)
+
+	if err != nil {
+		return err
+	}
+	engine.Message(prevRoom, "left room", character.Name, session)
+	engine.Message(character.currentRoom.GetName(), "entered room", character.Name, session)
+	return nil
+}
+
+func look(engine *akevitt.Akevitt, session akevitt.ActiveSession, arguments string) error {
+	sess, ok := session.(*ActiveSession)
+
+	if !ok {
+		return errors.New("could not cast to session")
+	}
+	if strings.TrimSpace(arguments) == "" {
+		for _, v := range engine.Lookup(sess.character.currentRoom) {
+			AppendText(sess, fmt.Sprintf("%s\n\t%s\n", v.GetName(), v.GetDescription()), sess.chat)
+		}
+
+		return nil
+	}
+
+	for _, v := range engine.Lookup(sess.character.currentRoom) {
+		if strings.EqualFold(v.GetName(), arguments) {
+			AppendText(sess, fmt.Sprintf("%s\n\t%s\n", v.GetName(), v.GetDescription()), sess.chat)
+		}
+	}
+	return nil
+}
+
+func interact(engine *akevitt.Akevitt, session akevitt.ActiveSession, arguments string) error {
+	sess, ok := session.(*ActiveSession)
+
+	if !ok {
+		return errors.New("invalid session type")
+	}
+
+	arguments = strings.TrimSpace(arguments)
+	interactables := akevitt.LookupOfType[Interactable](sess.character.currentRoom)
+	for _, v := range interactables {
+		if !strings.EqualFold(v.GetName(), arguments) {
+			continue
+		}
+
+		return v.Interact(engine, sess)
+	}
+
+	return fmt.Errorf("the object %s not found", arguments)
+}
+
+func say(engine *akevitt.Akevitt, session akevitt.ActiveSession, arguments string) error {
+	sess, ok := session.(*ActiveSession)
+
+	if !ok {
+		return errors.New("invalid session type")
+	}
+
+	return engine.Message(sess.character.currentRoom.GetName(), arguments, sess.character.Name, session)
+}
+
+func ooc(engine *akevitt.Akevitt, session akevitt.ActiveSession, command string) error {
+	return engine.Message("ooc", command, session.GetAccount().Username, session)
+}
+
+func backpack(engine *akevitt.Akevitt, session akevitt.ActiveSession, arguments string) error {
+	sess, ok := session.(*ActiveSession)
+
+	if !ok {
+		return errors.New("could not cast to session")
+	}
+
+	AppendText(sess, "Your backpack", sess.chat)
+	for k, v := range sess.character.Inventory {
+		AppendText(sess, fmt.Sprintf("№%d %s\n\t%s", k, v.GetName(), v.GetDescription()), sess.chat)
+	}
+	AppendText(sess, strings.Repeat("=.=", 16), sess.chat)
+
+	return nil
+}
