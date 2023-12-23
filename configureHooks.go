8813e4fb49a11f6eef3b4088d565eef72ad0bb3c
IvanKorchmit <criperadmiral92@gmail.com>
2023-12-23 13:32:06 +0200

    fix hooks

--- configureHooks.go
+++ configureHooks.go
@@ -1,24 +1,24 @@
 package akevitt
 
 // Accepts the function which gets invoked when someone sends the message (engine.Message)
-func (engine *Akevitt) UseOnMessage(f MessageFunc) *Akevitt {
-	engine.onMessage = f
+func (builder *akevittBuilder) UseOnMessage(f MessageFunc) *akevittBuilder {
+	builder.engine.onMessage = f
 
-	return engine
+	return builder
 }
 
 // Called when engine.Dialogue is called
-func (engine *Akevitt) UseOnDialogue(f DialogueFunc) *Akevitt {
-	engine.onDialogue = f
+func (builder *akevittBuilder) UseOnDialogue(f DialogueFunc) *akevittBuilder {
+	builder.engine.onDialogue = f
 
-	return engine
+	return builder
 }
 
 // Accepts function which gets called when the user lefts the game.
 // Note: use with caution, because calling methods from the engine like Message
 // will cause an infinite recursion
 // and in result: the application will crash.
-func (engine *Akevitt) UseOnSessionEnd(f DeadSessionFunc) *Akevitt {
-	engine.onDeadSession = f
-	return engine
+func (builder *akevittBuilder) UseOnSessionEnd(f DeadSessionFunc) *akevittBuilder {
+	builder.engine.onDeadSession = f
+	return builder
 }
