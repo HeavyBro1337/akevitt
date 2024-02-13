c459a0e8eb55d3c12708aeefe242c1b177d30ae1
IvanKorchmit <criperadmiral92@gmail.com>
2024-02-13 22:18:05 +0200

    message plugin

--- plugins/messagePlugin.go
+++ plugins/messagePlugin.go
@@ -6,6 +6,8 @@ import (
 	"github.com/IvanKorchmit/akevitt"
 )
 
+const MessagePluginData string = "MessagePlugin"
+
 type MessageFunc = func(engine *akevitt.Akevitt, session *akevitt.ActiveSession, channel, message, username string) error
 
 type MessagePlugin struct {
@@ -21,6 +23,12 @@ func (plugin *MessagePlugin) Message(engine *akevitt.Akevitt, channel, message, 
 	akevitt.PurgeDeadSessions(engine, engine.GetOnDeadSession())
 
 	for _, v := range engine.GetSessions() {
+		channels := v.Data[MessagePluginData].([]string)
+
+		if !akevitt.Find(channels, channel) {
+			continue
+		}
+
 		err := plugin.onMessageFn(engine, v, channel, message, username)
 
 		if session != v {
@@ -39,7 +47,9 @@ func (plugin *MessagePlugin) Build(engine *akevitt.Akevitt) error {
 	if plugin.includeCmd {
 		engine.AddCommand("ooc", plugin.oocCmd)
 	}
-
+	engine.AddInit(func(session *akevitt.ActiveSession) {
+		session.Data[MessagePluginData] = []string{"ooc"}
+	})
 	return nil
 }
 
