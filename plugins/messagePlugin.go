70020e027377d496ac1ffa7ac8bab7a17aa4abd6
IvanKorchmit <criperadmiral92@gmail.com>
2024-02-14 20:05:51 +0200

    hotfix

--- plugins/messagePlugin.go
+++ plugins/messagePlugin.go
@@ -61,7 +61,7 @@ func (plugin *MessagePlugin) Build(engine *akevitt.Akevitt) error {
 
 func NewMessagePlugin(includeCmd bool, fn MessageFunc, format string) *MessagePlugin {
 	if format == "" {
-		format = "%[1]s (%[1]s) says %[2]s"
+		format = "%s (%s) says %s"
 	}
 
 	return &MessagePlugin{
