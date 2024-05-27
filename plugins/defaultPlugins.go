928698e859e8c5b1ef74927226d764340842b9e7
IvanKorchmit <criperadmiral92@gmail.com>
2024-05-28 00:45:46 +0300

    remove unused code

--- plugins/defaultPlugins.go
+++ plugins/defaultPlugins.go
@@ -6,40 +6,6 @@ import (
 	"github.com/IvanKorchmit/akevitt"
 )
 
-// type DefaultPlugins struct {
-// 	Messages   *MessagePlugin
-// 	HeartBeats *HeartBeatsPlugin
-// 	plugins    []akevitt.Plugin
-// }
-
-// func (plugin *DefaultPlugins) Build(engine *akevitt.Akevitt) error {
-// 	for _, p := range plugin.plugins {
-// 		if err := p.Build(engine); err != nil {
-// 			return err
-// 		}
-// 		engine.AddPlugin(p)
-// 	}
-// 	return nil
-// }
-
-// func NewDefaultPlugins() *DefaultPlugins {
-// 	result := &DefaultPlugins{plugins: make([]akevitt.Plugin, 0)}
-
-// 	result.Messages = NewMessagePlugin(true, nil, "")
-
-// 	result.HeartBeats = NewHeartbeatPlugin().
-// 		NewDuration(time.Second).
-// 		NewDuration(time.Second * 10).
-// 		NewDuration(time.Second * 30).
-// 		NewDuration(time.Minute).
-// 		Finish()
-
-// 	result.plugins = append(result.plugins, result.Messages)
-// 	result.plugins = append(result.plugins, result.HeartBeats)
-
-// 	return result
-// }
-
 func DefaultPlugins() []akevitt.Plugin {
 	result := make([]akevitt.Plugin, 0)
 
