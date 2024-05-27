7b67605581718f99c5db8271bfeb2819b5142875
IvanKorchmit <criperadmiral92@gmail.com>
2024-05-28 00:00:44 +0300

    add example

--- example/main.go
+++ example/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"log"
+
 	"github.com/IvanKorchmit/akevitt"
 	"github.com/IvanKorchmit/akevitt/plugins"
+	"github.com/rivo/tview"
 )
 
 func main() {
-	akevitt.NewEngine().AddPlugin(plugins.DefaultPlugins()...)
+	room := akevitt.Room{
+		Name: "LOOL",
+	}
+
+	app := akevitt.NewEngine().
+		AddPlugin(plugins.DefaultPlugins()...).
+		UseSpawnRoom(&room).
+		UseRootUI(func(engine *akevitt.Akevitt, session *akevitt.ActiveSession) tview.Primitive {
+			return tview.NewTextView().SetText("Hello, World!")
+		}).
+		UseBind(":2222").
+		Finish()
+
+	log.Fatal(app.Run())
 }
