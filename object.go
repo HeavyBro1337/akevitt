6005bf3c94f3d5d47244623a8dcaa5d366e340ac
IvanKorchmit <criperadmiral92@gmail.com>
2024-05-29 00:05:37 +0300

    improve room

--- object.go
+++ object.go
@@ -1,5 +1,7 @@
 package akevitt
 
+import "fmt"
+
 type Object interface {
 	GetName() string
 }
@@ -8,7 +10,21 @@ type Room struct {
 	Name       string
 	Exits      []*Exit
 	Objects    []Object
-	OnPreEnter func(*Akevitt, *ActiveSession) error
+	OnPreEnter func(*Akevitt, *ActiveSession, *Exit) error
+}
+
+func (room *Room) Enter(engine *Akevitt, session *ActiveSession, targetExit *Exit) error {
+	belongs := Find(room.Exits, targetExit)
+
+	if !belongs {
+		return fmt.Errorf("the exit %s does not belong to %s", targetExit.Name, room.Name)
+	}
+
+	if room.OnPreEnter != nil {
+		return room.OnPreEnter(engine, session, targetExit)
+	}
+
+	return nil
 }
 
 func (room *Room) GetKey() uint64 {
