1fad17e8e5e2c2b5010792d9384d4ea5499ea081
Ivan Korchmit <criperadmiral92@gmail.com>
2024-12-28 01:38:15 +0200

    add spaces in-between

--- logging.go
+++ logging.go
@@ -10,9 +10,11 @@ const format string = "[%s] %s: %s\n"
 func LogInfo(message string) {
 	fmt.Printf(format, time.Now(), "LOG", message)
 }
+
 func LogWarn(message string) {
 	fmt.Printf(format, time.Now(), "WARN", message)
 }
+
 func LogError(message string) {
 	fmt.Printf(format, time.Now(), "ERR", message)
 }
