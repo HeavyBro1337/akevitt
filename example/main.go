941b85077658e039218abbe5b4f731d86240b9e0
Ivan Korchmit <criperadmiral92@gmail.com>
2024-12-28 01:39:09 +0200

    Parse HTML in the root UI

--- example/main.go
+++ example/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
+	"io"
 	"log"
+	"math/rand"
+	"os"
+	"strings"
 
 	"github.com/IvanKorchmit/akevitt"
 	"github.com/IvanKorchmit/akevitt/plugins"
+	"github.com/IvanKorchmit/akevitt/ui"
 	"github.com/rivo/tview"
 )
 
@@ -18,6 +25,7 @@ func main() {
 		AddPlugin(plugins.NewBoltPlugin("database.db")).
 		UseSpawnRoom(&room).
 		UseRootUI(Root).
+		UseMouse().
 		UseBind(":1999").
 		Finish()
 
@@ -25,7 +33,23 @@ func main() {
 }
 
 func Root(engine *akevitt.Akevitt, session *akevitt.ActiveSession) tview.Primitive {
-	modal := tview.NewModal().AddButtons([]string{"Go!"})
+	// Loading HTML. Can be simplified or be a separate function, whatever.
+	f, _ := os.ReadFile("./grid.html")
+	r := bytes.NewReader(f)
+	templ := template.New("html")
+	builder := &strings.Builder{}
+	io.Copy(builder, r)
+	templ.Parse(builder.String())
+	builder.Reset()
+	// Executing templates
+	templ.Execute(builder, map[string]any{
+		"randomNumber": rand.Int(),
+	})
+	dom, _ := ui.ParseHTML(strings.NewReader(builder.String()), session.Application)
+	// Registering callbacks
+	dom.OnCallback("exit", func() {
+		session.Kill(engine)
+	})
 
-	return modal
+	return dom.Primitive()
 }
